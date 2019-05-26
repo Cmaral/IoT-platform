b903488a45dd44a2444fc1157f4c202a7ea887eb
Cmaral <claumartal@gmail.com>
2019-05-26 15:55:21 +0200

    first commit

---
+++ readSerialXbee.py
@@ -0,0 +1,16 @@
+import serial
+from xbee import XBee, ZigBee
+
+PORT = '/dev/ttyUSB0'
+BAUDRATE = 115200
+
+serial_port = serial.Serial(PORT, BAUDRATE)
+xbee = XBee(serial_port,escaped=True)
+
+while True:
+    try:
+        print (xbee.wait_read_frame())
+    except KeyboardInterrupt:
+        break
+
+serial_port.close()
