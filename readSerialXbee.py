fe6f3f3061f35b20c01b4b37e9acf1ec23cc1449
Cmaral <claumartal@gmail.com>
2019-05-28 19:32:20 +0200

    Read values

--- readSerialXbee.py
+++ readSerialXbee.py
@@ -1,20 +1,29 @@
+# Receives frames from Waspmote Smart Cities board with Temperature, Humidity and Luminosity sensors
+# Communication via Xbee PRO S1 802.15.4
+
 import serial, json, re
 from xbee import XBee
 
-PORT = '/dev/ttyUSB0'
+PORT = '/dev/ttyUSB2'
 BAUDRATE = 115200
 
 serial_port = serial.Serial(PORT, BAUDRATE)
 xbee = XBee(serial_port,escaped=True)
 
-# Function to obtain values from frame and store them in JSON file
+# Function obtains values (Temperature, Humidity, Luminosity) from frame and stores them in JSON file
 def process_frame(frame):
     data = frame['rf_data']
     a,b,nodeId,frameId,luminosity,humidity,temperature,h = data.split(b'#')
-    print(humidity[5:].decode(),temperature[4:].decode(),luminosity[4:].decode()) 
+    
+    temperature = temperature[4:].decode() # Celsius
+    humidity = humidity[5:].decode() # 0-100 range
+    luminosity = luminosity[4:].decode() # 0-100 range
+    
+    print(temperature, humidity, luminosity) 
 
 
-# Main function, read frames from serialport PORT
+# Main function
+# Reads frames from serialport PORT
 def main():
     while True:
         try:
